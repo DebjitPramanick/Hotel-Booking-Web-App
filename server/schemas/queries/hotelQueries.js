7a145d1fddb3ec5ac0fc5be103ea1d68773fb3fd
DebjitPramanick <dpramanick9990@gmail.com>
2021-11-16 11:13:24 +0530

    Search hotels

--- server/schemas/queries/hotelQueries.js
+++ server/schemas/queries/hotelQueries.js
@@ -2,13 +2,14 @@ const graphql = require('graphql')
 const { UserType, HotelType } = require("../Type.js")
 const Hotel = require("../../models/Hotel.js")
 
-
+const GraphQLDate = require('graphql-date')
 const {
     GraphQLID,
     GraphQLString,
     GraphQLInt,
     GraphQLNonNull,
     GraphQLObjectType,
+    GraphQLInputObjectType,
     GraphQLList
 } = graphql
 
@@ -34,6 +35,33 @@ const getAllHotels = { // For getting hotel details
     async resolve(parent, args, req) {
         let hotels = await Hotel.find({ })
         return hotels
+    }
+}
+
+const searchHotels = { // For searching available hotels
+    type: HotelType,
+    args: {
+        location: { type: new GraphQLNonNull(GraphQLID) },
+        from: { type: new GraphQLNonNull(GraphQLDate) },
+        to: { type: new GraphQLNonNull(GraphQLDate) },
+        people: {
+            type: new GraphQLInputObjectType({
+                name: "people",
+                fields: {
+                    children: { type: GraphQLInt },
+                    adults: { type: GraphQLInt },
+                }
+            })
+        },
+    },
+    async resolve(parent, args, req) {
+        if (!args.id) {
+            throw new Error("Hotel ID is required.")
+        }
+        else {
+            let hotel = await Hotel.findById(args.id)
+            return hotel
+        }
     }
 }
 
