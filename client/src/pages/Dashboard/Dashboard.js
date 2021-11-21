55b59dee7edf04a78be064675bb641d540094733
DebjitPramanick <dpramanick9990@gmail.com>
2021-11-21 18:25:37 +0530

    Dashboard styles

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react'
 import styled from 'styled-components'
 import { GlobalContext } from '../../utils/Context'
 import RoomsList from './RoomsList'
+import HotelIMG from "../../assets/hotel.jpeg"
 
 const QuickView = styled.div`
     display: grid;
@@ -11,12 +12,37 @@ const QuickView = styled.div`
 const Info = styled.div`
     width: 100%;
     height: 100%;
-    padding: 20px;
     border-radius: 20px;
     background: grey;
     margin-right: 10px;
     max-width: -webkit-fill-available;
+    position: relative;
+    overflow: hidden;
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    .overlay{
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        background: #00000066;
+    }
 `
+const Controls = styled.div`
+    width: 100%;
+    position: absolute;
+    padding: 20px;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #fff
+`
+
 const Graph = styled.div`
     width: 100%;
     padding: 20px;
@@ -36,7 +62,14 @@ const Dashboard = () => {
     return (
         <div>
             <QuickView>
-                <Info />
+                <Info style={{backgroundImage: `url(${HotelIMG})`, textAlign: 'center'}}>
+                    <div className="overlay"></div>
+                    <Controls>
+                        <p>Create Room</p>
+                        <p>Update Room</p>
+                        <p>Update Hotel</p>
+                    </Controls>
+                </Info>
                 <Graph />
             </QuickView>
             <RoomsList />
