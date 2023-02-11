9970b3af82793affe06738cbd940bce491559f99
Debjit Pramanick <73888326+DebjitPramanick@users.noreply.github.com>
2023-02-12 02:58:35 +0530

    Update Firebase.js

--- client/src/utils/Firebase.js
+++ client/src/utils/Firebase.js
@@ -5,14 +5,7 @@ import {getStorage, ref} from 'firebase/storage'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyA3VHRbj4knFBXK3YVFdsr7xn2MvE1A0_E",
-    authDomain: "hotel-booking-app-2c070.firebaseapp.com",
-    projectId: "hotel-booking-app-2c070",
-    storageBucket: "hotel-booking-app-2c070.appspot.com",
-    messagingSenderId: "308536271440",
-    appId: "1:308536271440:web:cf389c892a5fa653486755"
-};
+const firebaseConfig = PASTE_FIREBASE_CONFIG;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
