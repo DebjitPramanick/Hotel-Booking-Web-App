d4102decadc59b21b8661eaaa43d27140ac72245
DebjitPramanick <dpramanick9990@gmail.com>
2023-02-14 23:33:51 +0530

    Firebase config for deploying

--- client/src/utils/Firebase.js
+++ client/src/utils/Firebase.js
@@ -5,7 +5,14 @@ import {getStorage, ref} from 'firebase/storage'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = PASTE_FIREBASE_CONFIG;
+const firebaseConfig = {
+    apiKey: "AIzaSyA3VHRbj4knFBXK3YVFdsr7xn2MvE1A0_E",
+    authDomain: "hotel-booking-app-2c070.firebaseapp.com",
+    projectId: "hotel-booking-app-2c070",
+    storageBucket: "hotel-booking-app-2c070.appspot.com",
+    messagingSenderId: "308536271440",
+    appId: "1:308536271440:web:cf389c892a5fa653486755"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
